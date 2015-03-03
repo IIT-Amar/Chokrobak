78295fd068d8972379e862a548f72a72dfe3db91
kazi <kaziiit@gmail.com>
2015-03-04 02:21:45 +0600

    navigation commit

---
+++ application/views/home.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="kazisrabon">
+    <link href="http://localhost/chokrobak/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://localhost/chokrobak/assets/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="page-container">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <a href="#"><img class="img-responsive img-portfolio img-hover" src="http://localhost/chokrobak/assets/images/baby1.jpg" alt=""></a>
+            </div>
+            <div class="col-md-4">
+            </div>
+            <div class="col-md-4">
+                <img class="img-responsive img-portfolio img-hover" src="http://localhost/chokrobak/assets/images/baby1.jpg" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-1">
+                <h3 style="display: inline;">শিরোনাম:</h3>
+            </div>
+            <div class="col-md-10">
+                <marquee>
+                    <h3 style="display: inline;"> শিরোনাম১ </h3>
+                    <h3 style="display: inline;"> শিরোনাম২ </h3>
+                    <h3 style="display: inline;"> শিরোনাম৩ </h3>
+                    <h3 style="display: inline;"> শিরোনাম৪ </h3>
+                    <h3 style="display: inline;"> শিরোনাম৫ </h3>
+                    <h3 style="display: inline;"> শিরোনাম৬ </h3>
+                    <h3 style="display: inline;"> শিরোনাম৭ </h3>
+                    <h3 style="display: inline;"> শিরোনাম৮ </h3>
+                    <h3 style="display: inline;"> শিরোনাম৯ </h3>
+                    <h3 style="display: inline;"> শিরোনাম১০ </h3>
+                    <h3 style="display: inline;"> শিরোনাম১১ </h3>
+                    <h3 style="display: inline;"> শিরোনাম১২ </h3>
+                </marquee>
+            </div>
+        </div>
+        <div class="row">
+            <nav class="navbar navbar-inverse">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">চক্রবাক</a>
+                    </div>
+
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li><a href="index.html">Home</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Products</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">MAXXIS</a></li>
+                                    <li><a href="#">PHP Tutorials</a></li>
+                                    <li><a href="#">Javascript Tutorials</a></li>
+                                    <li><a href="#">CSS Tutorials</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="about.html">About us</a></li>
+                            <li><a href="contacts.html">Contacts</a></li>
+                            <li><a href="photoGallary.html">Photo Gallary</a></li>
+                        </ul>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+<!--                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-9">-->
+<!--                        <ul class="nav navbar-nav">-->
+<!--                            <li class="active">-->
+<!--                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">হোম</a>-->
+<!--                                <ul class="dropdown-menu">-->
+<!--                                    <li><a href="#">Action</a></li>-->
+<!--                                    <li><a href="#">Another action</a></li>-->
+<!--                                    <li><a href="#">Something else here</a></li>-->
+<!--                                    <li class="divider"></li>-->
+<!--                                    <li><a href="#">Separated link</a></li>-->
+<!--                                </ul>-->
+<!--                            </li>-->
+<!--                            <li><a href="#">পত্রিকা</a></li>-->
+<!--<!--                            <li><a href="#">Link</a></li>-->-->
+<!--                        </ul>-->
+<!--                    </div>                    -->
+                </div><!-- /.container-fluid -->
+            </nav>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <ul class="nav nav-pills nav-stacked">
+                    <li role="presentation" class="active"><a href="#">Home</a></li>
+                    <li role="presentation"><a href="#">Home</a></li>
+                    <li role="presentation"><a href="#">Home</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="http://localhost/chokrobak/assets/js/bootstrap.js"></script>
+<script src="http://localhost/chokrobak/assets/js/jquery.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="http://localhost/chokrobak/assets/js/bootstrap.min.js"></script>
+</body>
+</html>
